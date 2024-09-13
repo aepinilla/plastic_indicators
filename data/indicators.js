2249e7046380ceea9f896add9d8ee1f1ed5b2104
Andres Pinilla Palacios <apinillapalacios@gmail.com>
2024-09-13 15:13:42 -0500

    initial

---
+++ data/indicators.js
@@ -0,0 +1,193 @@
+var indicatorsData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "indicator": "Population",
+            "name_in_database": "population",
+            "unit": "Number of people",
+            "description": "Number of inhabitants in that country",
+            "group": "socio-economic",
+            "sources": {
+                "text": "World Bank",
+                "url": "https://data.worldbank.org/indicator/SP.POP.TOTL"
+            }
+        },
+        {
+            "indicator": "Plastic domestic consumption",
+            "name_in_database": "plastic_domestic_consumption",
+            "unit": "tons/year",
+            "description": "Plastic domestic consumption per year in the country",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Plastic use for packaging applications",
+            "name_in_database": "plastic_use_packaging",
+            "unit": "tons/year",
+            "description": "From all the end-use applications, mass of plastics that are used in the packaging sector per year",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Total plastic waste generation",
+            "name_in_database": "plastic_waste_generation",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste generation per year",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Plastic waste collected",
+            "name_in_database": "plastic_waste_collected",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste collected per year",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Plastic waste uncollected",
+            "name_in_database": "plastic_waste_uncollected",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste uncollected per year",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Plastic waste openly burned at dumpsites",
+            "name_in_database": "plastic_openly_burned",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste that is openly burned on dumpsites",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Plastic waste sent to recycling",
+            "name_in_database": "plastic_recycling",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste sent to recycling per year",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Plastic waste leaked to marine environment",
+            "name_in_database": "plastic_leaked_marine",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste leaked to the marine environment per year, from land-sources",
+            "group": "plastic",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Marine-based activities plastic pollution",
+            "name_in_database": "marine_activities_plastic",
+            "unit": "tons/year",
+            "description": "Mass of plastic waste leaked to the marine environment per year from marine-based sources, such as commercial fishery, navigation actions, waste disposal from cruise ships, and shellfish/fish culture.",
+            "group": "plastic",
+            "sources": {
+                "text": "Tangled Seas: A Snapshot of Abandoned, Lost, or Otherwise Discarded Fishing Gear in South Asia",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099712409182331832/idu01c8db2110c4b90433d0837c0d714c942c8df"
+            }
+        },
+        {
+            "indicator": "Municipal solid waste generation",
+            "name_in_database": "msw_generation",
+            "unit": "tons/year",
+            "description": "Municipal solid waste generation in tons per year",
+            "group": "msw",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Collected municipal solid waste",
+            "name_in_database": "collected_msw",
+            "unit": "%",
+            "description": "Percentage of municipal solid waste that is collected",
+            "group": "msw",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Uncollected municipal solid waste",
+            "name_in_database": "uncollected_msw",
+            "unit": "%",
+            "description": "Percentage of municipal solid waste that is not collected",
+            "group": "msw",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Percentage of plastics in the municipal solid waste",
+            "name_in_database": "percentage_plastics_in_msw",
+            "unit": "%",
+            "description": "Percentage of plastics present in the municipal solid waste",
+            "group": "msw",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Municipal solid waste recycling",
+            "name_in_database": "msw_recycling",
+            "unit": "%",
+            "description": "Percentage of municipal solid waste that is recycled",
+            "group": "msw",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        {
+            "indicator": "Municipal solid waste openly burned at dumpsites",
+            "name_in_database": "msw_openly_burned",
+            "unit": "%",
+            "description": "Percentage of municipal solid waste that is openly burned on dumpsites",
+            "group": "msw",
+            "sources": {
+                "text": "World Bank. 2024. Waves of Plastic: A Snapshot of Marine Plastic Pollution in South Asia. Washington, D.C.: The World Bank.",
+                "url": "https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099933108202431142/idu18e0bbc271c078143281a4e016dc43e77b919"
+            }
+        },
+        // Rivers
+        {
+            "indicator": "Rivers",
+            "name_in_database": "rivers",
+            "unit": "",
+            "description": "Demo",
+            "group": "rivers",
+            "sources": {
+                "text": "Demo.",
+                "url": "demo"
+            }
+        },
+    ]
+}
